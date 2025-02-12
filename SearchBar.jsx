d559ec074ad3b3da9431d76dbe7ee7ce1cc228e6
Abishek0291-abi <abishek0291@gmail.com>
2025-02-12 20:35:19 +0530

    Create SearchBar.jsx

---
+++ SearchBar.jsx
@@ -0,0 +1,27 @@
+  javascript
+   import React, { useState } from 'react';
+   import { useDispatch } from 'react-redux';
+   import { fetchRecipes } from '../redux/slices/recipeSlice';
+
+   const SearchBar = () => {
+     const [query, setQuery] = useState('');
+     const dispatch = useDispatch();
+
+     const handleSearch = () => {
+       dispatch(fetchRecipes({ query }));
+     };
+
+     return (
+       <div>
+         <input
+           type="text"
+           value={query}
+           onChange={(e) => setQuery(e.target.value)}
+           placeholder="Search recipes..."
+         />
+         <button onClick={handleSearch}>Search</button>
+       </div>
+     );
+   };
+
+   export default SearchBar;
